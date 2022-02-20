eea8a8662917b2e4e508837a2b6dac5b213f1bff
Cloudy Young <cloudy.young@outlook.com>
2022-02-20 03:44:21 -0700

    Update data

--- src/App.js
+++ src/App.js
@@ -116,7 +116,7 @@ const App = () => {
                     </div>
                 </div>
             </Dialog>
-            <Checkout open={open} setOpen={setOpen} />
+            <Checkout open={open} setOpen={setOpen} data={getDistance() * transport.rate}/>
             <div className="fixed w-full h-full z-0">
                 <Map zoom={zoom} center={center}>
                     {startMarker.length !== 0 && <Marker width={50} anchor={startMarker}/>}
