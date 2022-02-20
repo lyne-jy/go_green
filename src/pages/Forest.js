be0b2959ae6dc9afb74b0753540888d66b6c8001
Cloudy Young <cloudy.young@outlook.com>
2022-02-19 22:27:51 -0700

    Update canvas

--- src/pages/Forest.js
+++ src/pages/Forest.js
@@ -11,7 +11,7 @@ import TreeCone from '../components/TreeCone';
 const Forest = () => {
     return (
         <Suspense fallback={<span>loading...</span>}>
-            <Canvas colorManagement camera={{ position: [50,50,500], fov: 2, zoom: 30, near: 30 }}>
+            <Canvas colorManagement camera={{ position: [50,50,5], fov: 2, zoom: 1, near: 30 }} className="canvas">
                 {/* <pointLight args={[`white`, 1, 100]} position={[-5, 5, 5]} /> */}
                 {/* <pointLight args={[`white`, 1, 100]} position={[65, 5, 5]} /> */}
                 <spotLight position={[0, 0, 120]} angle={0.3} />
