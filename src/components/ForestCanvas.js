46e699f26a2bfd968cfd3948fcf6c837bb7acb06
Cloudy Young <cloudy.young@outlook.com>
2022-02-20 01:31:58 -0700

    Add ForestCanvas background

--- src/components/ForestCanvas.js
+++ src/components/ForestCanvas.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Suspense } from 'react'
 import { Canvas } from '@react-three/fiber'
+import { Plane } from "@react-three/drei";
 
 import './SmallTreeIcon.css';
 import TreeCone from '../objects/TreeCone'
@@ -11,8 +12,12 @@ const ForestCanvas = () => {
     return (
         <Suspense fallback={ <></> }>
             <Canvas colorManagement camera={{ position: [45, 45, 45], fov: 2, zoom: 0.4, near: 30 }} className="small-tree-icon">
+                {/* <Plane args={[10, 10]} position={[0, 0, -100]}>
+                    <meshStandardMaterial color={"green"} />
+                </Plane> */}
+                <color attach="background" args={['#d6f9ff']} />
                 <spotLight position={[5, 5, 0]} angle={90} intensity={0.7} />
-                <spotLight position={[5, 5, 180]} angle={180} intensity={1} />
+                <spotLight position={[5, 5, 90]} angle={180} intensity={0.9} />
                 <ambientLight position={[1, 1, 1]} intensity={100} />
                 <TreeCone position={[0, -0.6, 0]} />
                 <TreeBlocks position={[1, -0.6, 0]} />
