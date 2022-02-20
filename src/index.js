6fa8e0b675a22175572d2bd31f43a04b4548eb9e
Sipeng He <hsp8412@hotmail.com>
2022-02-20 00:52:03 -0700

    Implement tree list

--- src/index.js
+++ src/index.js
@@ -1,29 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import './index.css';
-import App from './App';
+import "./index.css";
+import App from "./App";
 import Forest from "./pages/Forest";
-import Navbar from './components/Navbar';
+import Navbar from "./components/Navbar";
 
 function components(...components) {
-    return (
-        <>
-            {components.map(component => component)}
-        </>
-    );
+  return <>{components.map((component) => component)}</>;
 }
 
 ReactDOM.render(
-    <BrowserRouter>
-        <Routes>
-            <Route path="/" element={<App />} />
-            <Route path="/forest" element={components(<Navbar title="Forest" backButton />, <Forest />)} />
-        </Routes>
-    </BrowserRouter>,
-    document.getElementById('root')
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route
+        path="/forest"
+        element={components(<Navbar title="Forest" backButton />, <Forest />)}
+      />
+    </Routes>
+  </BrowserRouter>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
--- src/pages/Forest.js
+++ src/pages/Forest.js
@@ -1,9 +1,4 @@
 import React from "react";
-// import Navbar from "./components/nav";
-// import TreeTable from "../components/treeTable";
-// import TreeList from "../components/treeList";
-import SmallTreeIcon from "../components/SmallTreeIcon";
-import TreeCone from "../components/TreeCone";
 
 const trees = [
   {
@@ -26,21 +21,6 @@ const trees = [
   },
 ];
 
-const trees2 = [
-  {
-    treeId: 1,
-    date: "2022.02.19 13:00",
-  },
-  {
-    treeId: 2,
-    date: "2022.02.19 19:40",
-  },
-  {
-    treeId: 3,
-    date: "2022.02.19 19:40",
-  },
-];
-
 const Forest = () => {
   return (
     <>
@@ -58,52 +38,6 @@ const Forest = () => {
             </li>
           ))}
         </ul>
-      </div>
-
-      <div className="container mx-auto px-4">
-        <div className="flex flex-col">
-          <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-            <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
-              <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
-                <table className="min-w-full divide-y divide-gray-200">
-                  <thead className="bg-gray-50">
-                    <tr>
-                      <th
-                        scope="col"
-                        className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                      >
-                        Tree No.
-                      </th>
-                      <th
-                        scope="col"
-                        className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                      >
-                        Date
-                      </th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    {trees2.map((tree) => (
-                      <tr
-                        key={tree.treeId}
-                        className={
-                          tree.treeId % 2 === 0 ? "bg-white" : "bg-gray-50"
-                        }
-                      >
-                        <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
-                          {tree.treeId}
-                        </td>
-                        <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                          {tree.date}
-                        </td>
-                      </tr>
-                    ))}
-                  </tbody>
-                </table>
-              </div>
-            </div>
-          </div>
-        </div>
       </div>
     </>
   );
