8654e3c5c4ad412e5315df0353b9dbd17925360c
Cloudy Young <cloudy.young@outlook.com>
2022-02-19 19:18:38 -0700

    Update Forest.js

--- src/pages/Forest.js
+++ src/pages/Forest.js
@@ -6,15 +6,15 @@ import Box from "../components/Box";
 
 
 
+
 const Forest = () => {
-  return (
-      <Suspense fallback={<span>loading...</span>}>
-          <Canvas dpr={[1, 2]} camera={{ fov: 45, position: [-4, 2, -4] }}>
-              <color attach="background" args={['#1e2243']} />
-              <Box />
-          </Canvas>
-      </Suspense>
-  )
+    return (
+        <Suspense fallback={<span>loading...</span>}>
+            <Canvas dpr={[1, 2]} camera={{ fov: 45, position: [-4, 2, -4] }}>
+                <Box />
+            </Canvas>
+        </Suspense>
+    )
 }
 
 export default Forest
