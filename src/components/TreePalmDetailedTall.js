3b5a849b22bbbe91ce5b094037765aa9be8ec9d6
lybned <60757167+lybned@users.noreply.github.com>
2022-02-20 00:16:36 -0700

    Added 7 more trees

---
+++ src/components/TreePalmDetailedTall.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+import { TextureLoader } from 'three/src/loaders/TextureLoader'
+import { useLoader } from '@react-three/fiber'
+
+const TreePalmDetailedTall = () => {
+    const obj = useLoader(GLTFLoader, '/obj/tree_palmDetailedTall.glb')
+    const txt = useLoader(TextureLoader, '/obj/tree_palmDetailedTall.png')
+
+    return (
+        <mesh>
+            <primitive object={obj.scene} map={txt} />
+        </mesh>
+    )
+}
+
+export default TreePalmDetailedTall
